7cec19cb9061083bc12f6d42cebca2c71dcd25df
Mondli <mondli46401@gmail.com>
2025-07-24 08:45:03 +0200

    My first Python Project

---
+++ hello.py
@@ -0,0 +1,4 @@
+first_name = input("Please enter your first name: ").strip().title()
+surname = input("Please enter your surname: ").strip().title()
+
+print(f"\nHello, {first_name} {surname}. It is a pleasure to meet you!")
